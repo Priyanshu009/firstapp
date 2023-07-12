a0bc7ea5adef543f98968d369378603d5981d8ec
Priyanshu009 <priyanshu.parmar02@gmail.com>
2023-07-12 23:32:46 +0530

    first app 2

--- app/src/main/java/com/example/e4tjeyj/MainActivity.java
+++ app/src/main/java/com/example/e4tjeyj/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getText();
         setContentView(R.layout.activity_main);
+    }
+
+    private void getText() {
+
     }
 }
